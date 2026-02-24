6905e1143178a44200ed948cf7172c9c9a40d09e
Jotape <Tonicracko21@gmail.com>
2026-02-24 08:49:58 -0500

    Cambios actuales

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,47 @@
 let pedir_boton = document.querySelector(".boton_pedir");
 let plantar_boton = document.querySelector(".boton_plantarse");
 let contador_actual = document.querySelector(".contador_actual");
+let imagen_cartas = document.querySelector(".img_carta")
+let reiniciar_boton = document.querySelector(".boton_reiniciar")
+
+// let imagen_dealer = 
 
 let total = 0; 
 
 function sacar_carta() {
 
-    fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`)
         .then(function(response){
             return response.json(); 
         })
         .then(function(data){
+
+            imagen_cartas.src = data["cards"][0]["image"]
+            imagen_cartas.style = "translate(x): 20px;"
 
             let valor_carta = data["cards"][0]["value"];
 
             if (valor_carta === "KING" || valor_carta === "QUEEN" || valor_carta === "JACK") {
                 valor_carta = 10;
             } else if (valor_carta === "ACE") {
-                valor_carta = 11;
+                valor_carta = 1 || 11;
             } else {
                 valor_carta = parseInt(valor_carta);
             }
             total = total + valor_carta;
             contador_actual.innerHTML =
             "Actualmente estás en: " + total
+
+            if (total === 21) {
+                contador_actual.innerHTML =
+                "¡Felicidades, has ganado con los " + total + " puntos necesarios!";
+                pedir_boton.disabled = true;
+                plantar_boton.disabled = true 
+            }
+
             if (total > 21) {
                 contador_actual.innerHTML =
-                "Ha perdido, se pasó con: " + total;
+                "Has perdido, te pasaste con: " + total;
 
                 pedir_boton.disabled = true;
                 plantar_boton.disabled = true 
@@ -41,4 +56,9 @@ plantar_boton.addEventListener("click", function(){
     plantar_boton.disabled = true;
     contador_actual.innerHTML =
     "Te plantaste con: " + total + " puntos";
+    
 });
+
+reiniciar_boton.addEventListener("click", function(){
+    location.reload();
+})
